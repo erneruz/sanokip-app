041e89a58f543c6d89698c3e6df409b1134ea786
Ernest Ruzindana <erneruz@gmail.com>
2026-09-20 15:12:13 +0200

    updates

---
+++ src/components/partners.data.js
@@ -0,0 +1,11 @@
+import dynexarta from '../assets/partners/flipside.png'
+import mapmindLab from '../assets/partners/mapmindlab.png'
+import peakStar from '../assets/partners/intango.jpeg'
+
+export const partners = [
+  { name: 'Flipside', logo: flipside },
+  { name: 'MapMind Lab', logo: mapmindLab },
+  { name: 'Peak Star', logo: peakStar },
+]
+
+
